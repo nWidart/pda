4e850e381a38dc35da0705ca9bc799124c2f17ff
Nicolas Widart <n.widart@gmail.com>
2013-05-18 10:08:11 +0200

    Added margin right on navigation

--- app/tests/controllers/UsersControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class UsersControllerTest extends TestCase {
-	public function testIndex()
-	{
-		$response = $this->call('GET', 'dashboard');
-		$this->assertTrue($response->isOk());
-	}
-}
