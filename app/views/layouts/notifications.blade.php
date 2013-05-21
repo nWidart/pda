5ff5cb3afc95d86c4815611844e583ed1dd4a6f7
Nicolas Widart <n.widart@gmail.com>
2013-05-21 15:31:19 +0200

    Cleaned the notifications. Removed the .row

--- app/views/layouts/notifications.blade.php
+++ app/views/layouts/notifications.blade.php
@@ -1,57 +1,55 @@
-<div class="row">
-	@if (count($errors->all()) > 0)
-	<script type="text/javascript">
-		$(document).ready( function() {
-			alertify.error( 'Please check the form for errors.' );
-		});
-	</script>
-	@endif
+@if (count($errors->all()) > 0)
+<script type="text/javascript">
+	$(document).ready( function() {
+		alertify.error( 'Please check the form for errors.' );
+	});
+</script>
+@endif
 
-	@if ($message = Session::get('success'))
-	<script type="text/javascript">
-		$(document).ready( function() {
-			alertify.success( "<?php echo $message; ?>" );
-		});
-	</script>
-	@endif
+@if ($message = Session::get('success'))
+<script type="text/javascript">
+	$(document).ready( function() {
+		alertify.success( "<?php echo $message; ?>" );
+	});
+</script>
+@endif
 
-	@if ($message = Session::get('error'))
-	<script type="text/javascript">
-		$(document).ready( function() {
-			alertify.error( "<?php echo $message; ?>" );
-		});
-	</script>
-	@endif
+@if ($message = Session::get('error'))
+<script type="text/javascript">
+	$(document).ready( function() {
+		alertify.error( "<?php echo $message; ?>" );
+	});
+</script>
+@endif
 
-	@if( $message = Session::get('errorAlert') )
-	<script type="text/javascript">
-		$(document).ready( function() {
-			alertify.error( "<?php echo $message; ?>" );
-		});
-	</script>
-	@endif
+@if( $message = Session::get('errorAlert') )
+<script type="text/javascript">
+	$(document).ready( function() {
+		alertify.error( "<?php echo $message; ?>" );
+	});
+</script>
+@endif
 
-	@if ($message = Session::get('warning'))
-	<div class="alert alert-warning alert-block">
-		<button type="button" class="close" data-dismiss="alert">&times;</button>
-		<h4>Warning</h4>
-		{{{ $message }}}
-	</div>
-	@endif
-
-	@if ($message = Session::get('info'))
-	<div class="alert alert-info alert-block">
-		<button type="button" class="close" data-dismiss="alert">&times;</button>
-		<h4>Info</h4>
-		{{{ $message }}}
-	</div>
-	@endif
-	@if ( isset($notice) )
-	<script type="text/javascript">
-		$(document).ready( function() {
-			alertify.log( "<?php echo $notice; ?>", "", 0 );
-		});
-	</script>
-	@endif
-
+@if ($message = Session::get('warning'))
+<div class="alert alert-warning alert-block">
+	<button type="button" class="close" data-dismiss="alert">&times;</button>
+	<h4>Warning</h4>
+	{{{ $message }}}
 </div>
+@endif
+
+@if ($message = Session::get('info'))
+<div class="alert alert-info alert-block">
+	<button type="button" class="close" data-dismiss="alert">&times;</button>
+	<h4>Info</h4>
+	{{{ $message }}}
+</div>
+@endif
+@if ( isset($notice) )
+<script type="text/javascript">
+	$(document).ready( function() {
+		alertify.log( "<?php echo $notice; ?>", "", 0 );
+	});
+</script>
+@endif
+
