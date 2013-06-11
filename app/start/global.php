df2eca13621d9ea146396efd21f3dabb667bf024
Nicolas Widart <n.widart@gmail.com>
2013-06-11 20:43:33 +0200

    Added error pages.

--- app/start/global.php
+++ app/start/global.php
@@ -51,6 +51,21 @@ Log::useDailyFiles(storage_path().'/logs/'.$logFile);
 App::error(function(Exception $exception, $code)
 {
 	Log::error($exception);
+
+    if ( ! Config::get('app.debug'))
+    {
+        switch ($code)
+        {
+            case 403:
+                return Response::make(View::make('error/403'), 403);
+
+            case 500:
+                return Response::make(View::make('error/500'), 500);
+
+            default:
+                return Response::make(View::make('error/404'), 404);
+        }
+    }
 });
 
 /*
